ffb16d03f9bdbaf47cb08abcf80247321e81d317
xuyit <xu.yit@northeastern.edu>
2024-09-18 13:25:44 -0700

    page17 meet error

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import React from "react";
 import Labs from "./Labs";
+import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 export default function App() {
-  return (
-    <div>
-      <Labs />
-    </div>
-  );
+return (
+<HashRouter>
+<div>
+<Routes>
+<Route path="/" element={<Navigate to="Labs" />} />
+<Route path="/Labs/*" element={<Labs />} />
+</Routes>
+</div>
+</HashRouter>
+);
 }
