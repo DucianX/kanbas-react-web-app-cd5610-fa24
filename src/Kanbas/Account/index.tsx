fe8f485480588d7f4ac719f959b4595fb9affe24
xuyit <xu.yit@northeastern.edu>
2024-09-19 13:12:12 -0700

    till assginment

--- src/Kanbas/Account/index.tsx
+++ src/Kanbas/Account/index.tsx
@@ -1,20 +1,26 @@
-import { Routes, Route } from "react-router-dom";
-import { Navigate } from "react-router-dom";
-
-
-import Signin from "./Signin";
+import { Routes, Route, Navigate } from "react-router";
+import AccountNavigation from "./Navigation";
 import Profile from "./Profile";
+import Signin from "./Signin";
 import Signup from "./Signup";
-
 export default function Account() {
-    return (
+return (
     <div id="wd-account-screen">
-    <h2>Account</h2>
-    <Routes>
-    <Route path="/" element={<Navigate to="/Kanbas/Account/Signin" />} />
-    <Route path="/Signin" element={<Signin />} />
-    <Route path="/Profile" element={<Profile />} />
-    <Route path="/Signup" element={<Signup />} />
-    </Routes>
+        <table>
+            <tr>
+                <td valign="top">
+                    <AccountNavigation />
+                </td>
+                <td valign="top">
+                    <Routes>
+                        <Route path="/" element={<Navigate to="/Kanbas/Account/Signin" />} />
+                        <Route path="/Signin" element={<Signin />} />
+                        <Route path="/Profile" element={<Profile />} />
+                        <Route path="/Signup" element={<Signup />} />
+                    </Routes>
+                </td>
+            </tr>
+        </table>
     </div>
-    );}
+);
+}
