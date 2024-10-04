ada3ca3c444b1bfb4e2fd74aae6e3fcb97066c51
xuyit <xu.yit@northeastern.edu>
2024-10-03 20:30:25 -0700

    signin updated

--- src/Kanbas/Account/Signin.tsx
+++ src/Kanbas/Account/Signin.tsx
@@ -1,8 +1,12 @@
 import { Link } from "react-router-dom";
 export default function Signin() {
     return (
+        
         <div id="wd-signin-screen">
         <h3>Sign in</h3>
+        Yitian Xu's Labs
+        Section 03
+        https://github.com/DucianX/kanbas-react-web-app-cd5610-fa24
         <input id="wd-username" placeholder="username" className="form-control mb-2"/>
         <input id="wd-password" placeholder="password" className="form-control mb-2" type="password" /> 
         <Link id="wd-signin-btn" to="/Kanbas/Dashboard" className="btn btn-primary w-100">
