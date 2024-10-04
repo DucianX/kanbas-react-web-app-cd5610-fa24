a81f2b805446f8c9e9ec15c92a85daf190b3426c
xuyit <xu.yit@northeastern.edu>
2024-10-03 18:26:00 -0700

    working on assignment

--- src/Kanbas/Courses/Assignments/Editor.tsx
+++ src/Kanbas/Courses/Assignments/Editor.tsx
@@ -1,62 +1,80 @@
 export default function AssignmentEditor() {
     return (
-      <div id="wd-assignments-editor">
-        <label htmlFor="wd-name">Assignment Name</label> <br></br>
-        <br></br>
-        <input id="wd-name" value="A1 - ENV + HTML" /><br /><br />
-       
-        <textarea id="wd-description" cols={50} rows={10}>
-        The assignment is available online Submit a link to the landing page of your web application running on Netlify. The landing page should include the following: Your full name and section Links to each of the lab assignments Link to the Kanbas application Links to all relevant source code repositories The Kanbas application should include a link to navigate back to the landing page.
+      <div id="wd-assignments-editor" className="container p-4">
+      {/* Assignment Name */}
+      <div className="row mb-3">
+        <div className="col-12">
+          <label htmlFor="wd-name">Assignment Name</label>
+          <input id="wd-name" value="A1" className="form-control w-100 mb-3" />
+        </div>
+      </div>
 
-        </textarea>
+      {/* Description */}
+      <div className="row mb-3">
+        <div className="col-12">
+          <div className="form-control w-100 mb-3 p-3">
+            <p>The assignment is available online</p>
+            <p>
+              Submit a link to the landing page of your Web application running on Netlify.
+            </p>
+            <p>The landing page should include the following:</p>
+            <ul>
+              <li>Your full name and section</li>
+              <li>Links to each of the lab assignments</li>
+              <li>Link to the Kanbas application</li>
+              <li>Links to all relevant source code repositories</li>
+            </ul>
+            <p>
+              The Kanbas application should include a link to navigate back to the landing page.
+            </p>
+          </div>
+        </div>
+     
+   
+       
+
         <br></br>
         <br />
-        <table>
-          <tr>
-            <td align="right" valign="top">
-              <label htmlFor="wd-points">Points</label>
-            </td>
-            <td>
-              <input id="wd-points" value={100} />
-            </td>
-          </tr>
-          <br></br>
+        
+     
+          
+      {/* Points */}
+      <div className="row mb-3">
+        <div className="col-12 text-end">
+          <label htmlFor="wd-points" className="me-2">Points</label>
+          <input id="wd-points" value={100} className="form-control d-inline-block w-50" />
+        </div>
+      </div>
 
-          <tr>
-            <td align="right" valign="top">
+          
+        <div className="row mb-3">
+          <div className="col-12 text-end">
               <label htmlFor="wd-group">Assignment Group</label>
-            </td>
-            <td>
-              <select id="wd-group" defaultValue="ASSIGNMENTS">
-                <option value="ASSIGNMENTS">ASSIGNMENTS</option>
+              <select id="wd-group" className="form-control d-inline-block w-50" defaultValue="ASSIGNMENTS">
+              <option value="ASSIGNMENTS" className="form-control d-inline-block w-50">ASSIGNMENTS</option>
               </select>
-            </td>
-          </tr>
-          <br></br>
+          </div>
+        </div>
+        
 
-          <tr>
-            <td align="right" valign="top">
-              <label htmlFor="wd-display-grade-as">Display Grade as</label>
-            </td>
-            <td>
-            <select id="wd-display-grade-as" defaultValue="Percentage">
+        <div className="row mb-3">
+        <div className="col-12 text-end">
+           <label htmlFor="wd-display-grade-as">Display Grade as</label>
+            <select id="wd-display-grade-as" className="form-control d-inline-block w-50" defaultValue="Percentage">
                 <option value="Percentage">Percentage</option>
               </select>
-            </td>
-          </tr>
-          <br></br>
+              </div>
+              </div>
 
-          <tr>
-            <td align="right" valign="top">
-              <label htmlFor="wd-submission-type">Submission Type</label>
-            </td>
-            <td>
-            <select id="wd-submission-type" defaultValue="Online">
+        <div className="row mb-3">
+        <div className="col-12 text-end">
+            <label htmlFor="wd-submission-type">Submission Type</label>
+            <select id="wd-submission-type" className="form-control d-inline-block w-50" defaultValue="Online">
                 <option value="Online">Online</option>
               </select>
-            </td>
-          </tr>
-          <br></br>
+              </div>
+              </div>
+        
 
           <tr>
             <td align="right" valign="top">
@@ -161,8 +179,8 @@ export default function AssignmentEditor() {
           </tr>
           
           
-        </table>
-      </div>
+     
+      </div></div>
   );}
   
   
