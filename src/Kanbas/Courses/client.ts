3d18d9daac19e995acbae80aac8ffc51d2107d33
xuyit <xu.yit@northeastern.edu>
2024-12-10 17:50:28 -0800

    unable to del

--- src/Kanbas/Courses/client.ts
+++ src/Kanbas/Courses/client.ts
@@ -1,14 +1,22 @@
 import axios from "axios";
+const axiosWithCredentials = axios.create({ withCredentials: true });
+
 import {USERS_API} from "../Account/client";
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 // 根据URL里面只有course可以判断这里只和Courses相关。所以在courses的client.ts中安装是合理的
 const COURSES_API = `${REMOTE_SERVER}/api/courses`;
+
+export const createCourse = async (course: any) => {
+    const { data } = await axiosWithCredentials.post(COURSES_API, course);
+    return data;
+};
+
 export const fetchAllCourses = async () => {
-    const { data } = await axios.get(COURSES_API);
+    const { data } = await axiosWithCredentials.get(COURSES_API);
     return data;
 };
 export const deleteCourse = async (id: string) => {
-    const { data } = await axios.delete(`${COURSES_API}/${id}`);
+    const { data } = await axiosWithCredentials.delete(`${COURSES_API}/${id}`);
     return data;
 };
 export const updateCourse = async (course: any) => {
